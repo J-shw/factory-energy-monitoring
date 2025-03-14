ef7e20e8463debb00f098d289bd3c295b6006bd0
J-shw <91017208+J-shw@users.noreply.github.com>
2025-03-14 18:22:03 +0000

    Improved MQTT system

--- fake-data/app/main.py
+++ fake-data/app/main.py
@@ -4,12 +4,18 @@ import logging, time, os
 logging.basicConfig(level=logging.DEBUG)
 mqtt_host = os.environ.get("MQTT_BROKER_HOST")
 
-def send_mqtt(host, port, topic, message):
-  client = mqtt.Client()
-  client.connect(host, port)
+client_id = "fake_data_system"
+client = mqtt.Client(client_id=client_id)
+
+client.connect(mqtt_host, 1883, 60)
+
+def send_mqtt(topic, message):
+  logging.debug('Sending MQTT message')
   client.publish(topic, message)
-  client.disconnect()
+  
 
 while True:
-  send_mqtt(mqtt_host, 1883, "example-topic", "Hello, MQTT!")
+  send_mqtt("example-topic", "Hello, MQTT!")
   time.sleep(1)
+
+# client.disconnect()
