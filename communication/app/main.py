72578231d8387abe1b2e589283200b4f4b85e621
J-shw <91017208+J-shw@users.noreply.github.com>
2025-03-14 18:21:54 +0000

    Added websocket

--- communication/app/main.py
+++ communication/app/main.py
@@ -1,19 +1,26 @@
 import paho.mqtt.client as mqtt
-import logging, os
+import logging, os, socketio
 
 logging.basicConfig(level=logging.DEBUG)
 mqtt_host = os.environ.get("MQTT_BROKER_HOST")
+
+sio = socketio.Client()
+sio.connect('http://web:8080')
 
 def on_connect(client, userdata, flags, rc):
     logging.info("Connected with result code "+str(rc))
     client.subscribe("example-topic")
 
 def on_message(client, userdata, msg):
-    logging.debug(msg.topic+" "+str(msg.payload))
+    logging.debug(f"Client '{client._client_id.decode('utf-8')}' received: {msg.topic} {msg.payload}")
+    sio.emit('mqtt_message', {'topic': msg.topic, 'payload': msg.payload.decode('utf-8')})
 
-client = mqtt.Client()
+client_id = "communication_system"
+client = mqtt.Client(client_id=client_id)
+
 client.on_connect = on_connect
 client.on_message = on_message
+
 
 client.connect(mqtt_host, 1883, 60)
 
