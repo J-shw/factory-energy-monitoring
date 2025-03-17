74c0f49e7a363213084b853d5e9f916bf91c0d12
Josh <91017208+J-shw@users.noreply.github.com>
2025-03-17 17:00:24 +0000

    Create websocket.js

---
+++ web/app/static/script/websocket.js
@@ -0,0 +1,31 @@
+const socket = io('http://' + document.domain + ':8000', transports=['websocket']);
+
+socket.on('connect', function() {
+    console.log('Connected to server');
+    initializeChart();
+});
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
+
+socket.on('mqtt_message', function(data) {
+    console.log('Received message: ' + data.topic + ' ' + data.payload)
+    const messages = document.getElementById('messages');
+    const newMessage = document.createElement('li');
+    newMessage.textContent = `Topic: ${data.topic}, Payload: ${data.payload}`;
+    messages.appendChild(newMessage);
+
+     try {
+        const payload = JSON.parse(data.payload);
+        console.log(payload)
+        updateAmpsChart(payload);
+        updateVoltsChart(payload);
+    } catch (error) {
+        console.error('Error parsing payload:', error);
+    }
+});
+
+function sendMessage() {
+    socket.emit('web-message', 'From the web!');
+}
