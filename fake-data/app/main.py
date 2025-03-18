af128ce5ff2069a39ad9131467aa421744ff86a6
J-shw <91017208+J-shw@users.noreply.github.com>
2025-03-18 19:27:13 +0000

    Added attempt system

--- fake-data/app/main.py
+++ fake-data/app/main.py
@@ -13,24 +13,33 @@ def send_mqtt(topic, message):
   logging.debug(f'Sending MQTT message | Topic: {topic} | Message: {message}')
   client.publish(topic, message)
 
+attempts = 3
+timeout = 5
 devices = None
-try:
-  url = "http://devices:9002/devices/"
 
-  response = requests.get(url)
-  response.raise_for_status()
-  devices = response.json()
+for attempt in range(attempts):
+  logging.info(f'Attempting to load devices | Attempt {attempt}/{attempts}')
+  try:
+    url = "http://devices:9002/devices/"
 
-  logging.info(f"Response Status Code: {response.status_code}")
+    response = requests.get(url)
+    response.raise_for_status()
+    devices = response.json()
 
-except requests.exceptions.RequestException as e:
-  logging.error(f"Error: {e}")
-except json.JSONDecodeError:
-  logging.error("Response is not valid JSON")
-finally:
-  if devices is None or len(devices) == 0:
-    logging.warning("No devices returned")
-    devices = [{"name":"Fake Device","description":"Fake Sensor due to no devices being registered","id":"fake-id-here","dateCreated":"2025-03-18T12:18:03.884939Z"}]
+    logging.info(f"Devices loaded")
+
+  except requests.exceptions.RequestException as e:
+    logging.error(f"Error: {e}")
+    time.sleep(timeout)
+  except json.JSONDecodeError:
+    logging.error("Response is not valid JSON")
+    time.sleep(timeout)
+
+
+if devices is None or len(devices) == 0:
+  logging.warning("No devices returned")
+  devices = [{"name":"Fake Device","description":"Fake Sensor due to no devices being registered","id":"fake-id-here","dateCreated":"2025-03-18T12:18:03.884939Z"}]
+
 
 num_devices = len(devices)
 logging.info(f"Number of devices: {num_devices}")
