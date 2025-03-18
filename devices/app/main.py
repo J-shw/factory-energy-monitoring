3c610805d64f185f773af9251f72282bf0a481ee
J-shw <91017208+J-shw@users.noreply.github.com>
2025-03-18 12:19:00 +0000

    Uses new model

--- devices/app/main.py
+++ devices/app/main.py
@@ -1,21 +1,23 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
-from models import Item, SessionLocal
+from models import Device, SessionLocal
+from typing import Optional
+from uuid import UUID
 import datetime, uvicorn
 
 app = FastAPI()
 
-class ItemBase(BaseModel):
-    timestamp: datetime.datetime
-    amps: int
-    volts: int
+class DeviceCreate(BaseModel):
+    name: str
+    description: Optional[str] = None
 
-class ItemCreate(ItemBase):
-    pass
+class DeviceOut(DeviceCreate):
+    id: UUID
+    dateCreated: datetime.datetime
 
-class ItemOut(ItemBase):
-    id: int
+    class Config:
+        orm_mode = True
 
 def get_db():
     db = SessionLocal()
@@ -24,24 +26,24 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/device/", response_model=ItemOut)
-def create_item(item: ItemCreate, db: Session = Depends(get_db)):
-    db_item = Item(**item.dict())
+@app.post("/device/", response_model=DeviceOut)
+def create_item(item: DeviceCreate, db: Session = Depends(get_db)):
+    db_item = Device(**item.dict())
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
     return db_item
 
-@app.get("/devices/{item_id}", response_model=ItemOut)
+@app.get("/devices/{item_id}", response_model=DeviceOut)
 def read_item(item_id: int, db: Session = Depends(get_db)):
-    db_item = db.query(Item).filter(Item.id == item_id).first()
+    db_item = db.query(Device).filter(Device.id == item_id).first()
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return db_item
 
-@app.get("/devices/", response_model=list[ItemOut])
+@app.get("/devices/", response_model=list[DeviceOut])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = db.query(Item).offset(skip).limit(limit).all()
+    items = db.query(Device).offset(skip).limit(limit).all()
     return items
 
 @app.get("/")
