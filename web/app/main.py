1f529d782e5614f6efe18ac08b7cb74dbd8618a5
J-shw <91017208+J-shw@users.noreply.github.com>
2025-03-18 14:27:12 +0000

    Added devices route to request data from devices system

--- web/app/main.py
+++ web/app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify
 from waitress import serve
-import logging
+import logging, requests
 
 logging.basicConfig(level=logging.INFO)
 
@@ -9,6 +9,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/devices/<UID>')
+def device(UID):
+    try:
+        data = requests.get(f'http://devices:9002/devices/{UID}')
+        data.raise_for_status()
+        return jsonify(data.json()), 200
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500 
+    except ValueError:
+        return jsonify({"error": "Invalid Json response"}), 500
 
 @app.route('/health')
 def health():
