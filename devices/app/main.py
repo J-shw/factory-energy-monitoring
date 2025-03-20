ac22009ca634e79356e0ca5822b735c45ec8b401
J-shw <91017208+J-shw@users.noreply.github.com>
2025-03-20 11:32:53 +0000

    Renamed items to devices

--- devices/app/main.py
+++ devices/app/main.py
@@ -13,8 +13,8 @@ def get_db():
         db.close()
 
 @app.post("/device/", response_model=DeviceOut)
-def create_item(item: DeviceCreate, db: Session = Depends(get_db)):
-    db_item = Device(**item.dict())
+def create_device(device: DeviceCreate, db: Session = Depends(get_db)):
+    db_item = Device(**device.dict())
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
@@ -24,13 +24,13 @@ def create_item(item: DeviceCreate, db: Session = Depends(get_db)):
 def read_item(item_id: str, db: Session = Depends(get_db)):
     db_item = db.query(Device).filter(Device.id == item_id).first()
     if db_item is None:
-        raise HTTPException(status_code=404, detail="Item not found")
+        raise HTTPException(status_code=404, detail="Device not found")
     return db_item
 
 @app.get("/devices/", response_model=list[DeviceOut])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = db.query(Device).offset(skip).limit(limit).all()
-    return items
+    devices = db.query(Device).offset(skip).limit(limit).all()
+    return devices
 
 @app.get("/")
 async def root():
