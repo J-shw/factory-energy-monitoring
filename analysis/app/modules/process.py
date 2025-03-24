566ecf5b07ee7cbdf2eb598a0936f06071ec8339
Josh <91017208+J-shw@users.noreply.github.com>
2025-03-24 16:01:15 +0000

    Uses limit data to process data

--- analysis/app/modules/process.py
+++ analysis/app/modules/process.py
@@ -1,8 +1,5 @@
-def energy_data(volts: float, amps: float) -> tuple[bool, bool]:
-    VOLTAGE_THRESHOLD_HIGH = 120 * 1.09
-    VOLTAGE_THRESHOLD_LOW = 120 * 0.91
-    AMP_THRESHOLD = 10
+def energy_data(volts: float, amps: float, limit) -> tuple[bool, bool]:
 
-    highLowVoltage = volts > VOLTAGE_THRESHOLD_HIGH or volts < VOLTAGE_THRESHOLD_LOW
-    overCurrent = amps > AMP_THRESHOLD
+    highLowVoltage = volts > limit.highVoltageValue or volts < limit.lowVoltageValue
+    overCurrent = amps > limit.overCurrentValue
     return highLowVoltage, overCurrent
