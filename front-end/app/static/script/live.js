84b2ccd96a58e8b2ba16f44125af7e59bb20fcd9
J-shw <91017208+J-shw@users.noreply.github.com>
2025-04-30 15:59:23 +0100

    Create live.js

---
+++ front-end/app/static/script/live.js
@@ -0,0 +1,29 @@
+const iotSelect = document.getElementById('iot-select');
+
+fetch('/get/iot')
+.then(response => {
+    if (!response.ok) {
+    throw new Error(`HTTP error - status: ${response.status}`);
+    }
+    return response.json();
+})
+.then(data => {
+    console.log('Iot data:', data);
+    data.forEach(iot => {
+        let option = document.createElement('option')
+        option.value = iot.id;
+        option.textContent = iot.name;
+        iotSelect.appendChild(option);
+    });
+
+
+})
+.catch(error => {
+    console.error('Failed to fetch iot data:', error);
+});
+
+iotSelect.addEventListener('change', (event) => {
+    const iotId = event.target.value;
+    console.log('New iot selected. ID - ', iotId);
+    
+  });
