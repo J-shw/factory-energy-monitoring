79d174ddb524acfb1f674c80602d68841131b16e
Josh <91017208+J-shw@users.noreply.github.com>
2025-05-08 09:48:45 +0100

    Route function name changes

--- device-management-system/app/main.py
+++ device-management-system/app/main.py
@@ -15,7 +15,7 @@ def get_db():
 # Entity APIs - - -
 
 @app.post("/create/entity/", response_model=EntityOut)
-def create_device(entity: EntityCreate, db: Session = Depends(get_db)):
+def create_entity(entity: EntityCreate, db: Session = Depends(get_db)):
     db_item = Entity(**entity.dict())
     db.add(db_item)
     db.commit()
@@ -34,21 +34,21 @@ def get_entity_by_iot_id(iot_id: str, db: Session = Depends(get_db)):
     return db_item
 
 @app.get("/get/entity/{entity_id}", response_model=EntityOut)
-def read_item(entity_id: str, db: Session = Depends(get_db)):
+def get_enetity_by_id(entity_id: str, db: Session = Depends(get_db)):
     db_item = db.query(Entity).filter(Entity.id == entity_id).first()
     if db_item is None:
         raise HTTPException(status_code=404, detail="Entity not found")
     return db_item
 
 @app.get("/get/entity/", response_model=list[EntityOut])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def get_entity(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     entities = db.query(Entity).offset(skip).limit(limit).all()
     return entities
 
 # IoT APIs - - -
 
 @app.post("/create/iot/", response_model=IotOut)
-def create_device(iot: IotCreate, db: Session = Depends(get_db)):
+def create_iot(iot: IotCreate, db: Session = Depends(get_db)):
     db_item = Iot(**iot.dict())
     db.add(db_item)
     db.commit()
@@ -56,14 +56,14 @@ def create_device(iot: IotCreate, db: Session = Depends(get_db)):
     return db_item
 
 @app.get("/get/iot/{iot_id}", response_model=IotOut)
-def read_item(iot_id: str, db: Session = Depends(get_db)):
+def get_iot_by_id(iot_id: str, db: Session = Depends(get_db)):
     db_item = db.query(Iot).filter(Iot.id == iot_id).first()
     if db_item is None:
         raise HTTPException(status_code=404, detail="Iot not found")
     return db_item
 
 @app.get("/get/iot/", response_model=list[IotOut])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def get_iot(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     iots = db.query(Iot).offset(skip).limit(limit).all()
     return iots
 
